d53d949f55f26dc749f3a7a668e00e6dc33cb10d
Ashley Lin <linashleyy@gmail.com>
2026-01-09 01:11:33 -0500

    Render delivery options for each cart item

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,43 @@
 import { cart } from "./data/cart.js";
 import { products } from "./data/products.js";
+import { deliveryOptions } from "./data/deliveryOptions.js";
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+
+function renderDeliveryOptions(cartItem) {
+  let deliveryOptionsHTML = "";
+
+  const today = dayjs();
+
+  deliveryOptions.forEach((option) => {
+    const deliveryDateObj = today.add(option.deliveryDays, "day");
+    const deliveryDate = deliveryDateObj.format("dddd, MMMM D");
+    const deliveryPrice =
+      option.priceCents === 0
+        ? "FREE"
+        : `$${(option.priceCents / 100).toFixed(2)} -`;
+
+    deliveryOptionsHTML += `
+      <div class="delivery-option">
+        <input
+          type="radio"
+          ${
+            cartItem.deliveryOptionId === option.deliveryOptionId
+              ? "checked"
+              : ""
+          }
+          class="delivery-option-input"
+          name="delivery-option-${cartItem.productId}"
+        />
+        <div>
+          <div class="delivery-option-date">${deliveryDate}</div>
+          <div class="delivery-option-price">${deliveryPrice} Shipping</div>
+        </div>
+      </div>
+    `;
+  });
+
+  return deliveryOptionsHTML;
+}
 
 function renderCartSummary() {
   let cartSummaryHTML = "";
@@ -48,42 +86,7 @@ function renderCartSummary() {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    ${cartItem.deliveryOptionId === "1" ? "checked" : ""}
-                    class="delivery-option-input"
-                    name="delivery-option-${cartItem.productId}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Tuesday, June 21</div>
-                    <div class="delivery-option-price">FREE Shipping</div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    ${cartItem.deliveryOptionId === "2" ? "checked" : ""}
-                    class="delivery-option-input"
-                    name="delivery-option-${cartItem.productId}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Wednesday, June 15</div>
-                    <div class="delivery-option-price">$4.99 - Shipping</div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    ${cartItem.deliveryOptionId === "3" ? "checked" : ""}
-                    class="delivery-option-input"
-                    name="delivery-option-${cartItem.productId}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Monday, June 13</div>
-                    <div class="delivery-option-price">$9.99 - Shipping</div>
-                  </div>
-                </div>
+                ${renderDeliveryOptions(cartItem)}
               </div>
             </div>
           </div>
